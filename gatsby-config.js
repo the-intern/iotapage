eaa6eeca259ea604fb3f947dbefdb83f3f362c2a
the-intern <ostclasses@gmail.com>
2023-01-16 10:01:26 -0600

    basic changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Iotapage`,
+    description: `Search here`,
     author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    siteUrl: `https://iotapage.vercel.app/`,
   },
   plugins: [
     `gatsby-plugin-image`,
