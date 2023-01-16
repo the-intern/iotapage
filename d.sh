825b6989e319c1ba1680e2773f99bf0f15a28c1c
the-intern <ostclasses@gmail.com>
2023-01-15 21:13:47 -0600

    gatsby ...

---
+++ d.sh
@@ -0,0 +1,7 @@
+echo "# iotapage" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:the-intern/iotapage.git
+git push -u origin main
